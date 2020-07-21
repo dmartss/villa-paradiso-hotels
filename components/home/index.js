54e9b7854c3182d4c778ea1e35d0b9a60cbfef03
Dan <dan@ariesliving.com>
2020-07-21 10:54:43 -0400

    home content

--- components/home/index.js
+++ components/home/index.js
@@ -3,11 +3,9 @@ import styles from './home.module.css'
 export default function Home() {
   return (
     <div className={styles.main}>
-      <h1 className="fw4">Fresh Nest Organizing</h1>
-      <h4 className="fw3">Your Professional Organizer</h4>
-      <div className={styles.content}>
-        <h2 className="fw5">Behind the Business</h2>
-      </div>
+      <h1 className="fw4">Villa Paradiso Apartment Hotel</h1>
+      <h4 className="fw3">By Aries Living</h4>
+      <div className={styles.content}></div>
     </div>
   )
 }
