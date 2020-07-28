9cd8abfe17ba1aa5f2b80858f128ab0457ace0ac
Dan <dan@ariesliving.com>
2020-07-28 16:51:45 -0400

    fix name

--- pages/about.js
+++ pages/about.js
@@ -1,7 +1,7 @@
 import Page from 'components/page'
 import About from 'components/about'
 
-export default function IndexPage() {
+export default function AboutPage() {
   return (
     <Page>
       <About />
