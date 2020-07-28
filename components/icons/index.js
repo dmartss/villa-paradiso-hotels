7a286e801b099991034c4ce7b8a602355a462daa
Dan <dan@ariesliving.com>
2020-07-28 16:32:01 -0400

    named exports

--- components/icons/index.js
+++ components/icons/index.js
@@ -1,4 +1,4 @@
-export { default as Logo } from './logo'
+export { default } from './logo'
 export { default as Instagram } from './instagram'
 export { default as Facebook } from './facebook'
 export { default as Twitter } from './twitter'
