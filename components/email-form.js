8a58cb99d87eee5044ff9cc3ba94b21be8f83ef3
Dan <dan@ariesliving.com>
2020-09-11 13:01:36 -0400

    forms

--- components/email-form.js
+++ components/email-form.js
@@ -1,266 +1,68 @@
-import { validate } from 'email-validator'
-import cn from 'classnames'
-import AutoComplete from './auto-complete'
-import Button from './button'
-import EmailClosedIcon from './icons/email-closed'
+export default function EmailForm({ flex }) {
+  const token = process.env.NEXT_PUBLIC_TOKEN
+  return (
+    <div
+      dangerouslySetInnerHTML={{
+        __html: `
+          <script type="text/javascript" src="//contact-api.inguest.com/bundles/revinatecontactapi/js/revinate-form.js?v=1"></script>
+          <form id="revinate_contact_api_form" token="${token}"
+                onsubmit="revFormOnSubmit(); event.preventDefault();">
+            <div class=${flex ? 'flex' : ''}>
+              <div>
+                <input placeholder="you@domain.com" class=${
+                  flex ? 'flex' : ''
+                } type="email" name="email" required/>
+              </div>
+              <div class="button">
+                <button class=${flex ? 'flex' : ''} type="submit">Subscribe</button>
+              </div>
+            </div>
 
-class EmailForm extends React.Component {
-  constructor(props) {
-    super(props)
-
-    this.state = {
-      defaultEmail: props.defaultEmail,
-      email: props.defaultEmail,
-      ios: null,
-      focus: false,
-      shaking: false,
-      error: false,
-      safari: false
-    }
-
-    this.inputRef = null
-  }
-
-  componentDidMount() {
-    const ua = navigator.userAgent
-    const ios = /iPad|iPhone|iPod/.test(ua)
-    const safari = Boolean(ua.match(/Version\/[\d.]+.*Safari/))
-
-    this.setState({ ios, safari })
-  }
-
-  onRef = input => {
-    this.inputRef = input
-
-    if (this.inputRef && this.props.autoFocus) {
-      if (this.props.defaultEmail != null) {
-        this.select()
-      } else {
-        this.focus()
-      }
-    }
-  }
-
-  onInput = email => {
-    if (this.state.error) if (validate(email)) this.setState({ error: false })
-
-    this.setState({ email })
-
-    if (this.props.onChange && typeof this.props.onChange === 'function') {
-      this.props.onChange({ email })
-    }
-  }
-
-  onFocus = () => {
-    this.setState({ focus: true })
-  }
-
-  onBlur = () => {
-    this.setState({ focus: false })
-  }
-
-  focus() {
-    // On mobile, we don't want to auto-focus
-    // in order to prevent zooming.
-    if (window.innerWidth < 992) return
-    if (this.inputRef) this.inputRef.focus()
-  }
-
-  select() {
-    this.inputRef.select()
-  }
-
-  reset() {
-    this.inputRef.reset()
-  }
-
-  submit = ev => {
-    if (ev) ev.preventDefault()
-
-    const { email } = this.state
-    const { onEmail } = this.props
-
-    if (validate(email)) {
-      this.setState({ error: false })
-      onEmail(email)
-    } else {
-      if (this.state.shaking) return
-
-      this.setState({ error: true, shaking: true })
-
-      setTimeout(() => {
-        this.setState({ shaking: false })
-        this.focus()
-      }, 1000)
-    }
-  }
-
-  render() {
-    const classes = []
-    const { ios, safari, error, focus, shaking, defaultEmail } = this.state
-    const { flex, loading, errorMessage, message, disabled } = this.props
-    if (ios) classes.push('ios')
-    if (safari) classes.push('safari-browser')
-
-    return (
-      <form
-        id="revinate_contact_api_form"
-        token="0a0bd4dc-ac51-487c-85ca-05d751d70287"
-        className={cn(classes)}
-        onSubmit={this.submit}
-      >
-        <div
-          className={cn('email', {
-            error: error || errorMessage,
-            focus: focus,
-            loading: loading,
-            shaking: shaking,
-            flex: flex
-          })}
-        >
-          <label htmlFor="email-input-field">
-            <span className="icon">
-              <EmailClosedIcon height="15" width="15" fill="currentColor" />
-            </span>
-            <AutoComplete
-              ref={this.onRef}
-              onFocus={this.onFocus}
-              onBlur={this.onBlur}
-              onInput={this.onInput}
-              align="left"
-            >
-              <input
-                type="email"
-                name="email"
-                id="email-input-field"
-                defaultValue={defaultEmail || ''}
-                disabled={Boolean(loading || disabled)}
-                placeholder={'you@domain.com'}
-                aria-label="Your email address"
-              />
-            </AutoComplete>
-          </label>
-
-          <Button type="submit" disabled={Boolean(loading || disabled)} width={flex && 175}>
-            {message}
-          </Button>
-        </div>
-
-        {errorMessage && (
-          <p className="error-message">
-            <span>
-              {typeof errorMessage === 'string'
-                ? errorMessage
-                : 'A network error has occurred. Please retry.'}
-            </span>
-          </p>
-        )}
-
-        <style jsx>{`
+          <style scoped>
           form {
             display: inline-block;
             position: relative;
             max-width: 100%;
           }
 
-          form > div {
+          form div {
             width: 240px;
             max-width: 100%;
+            text-align: center;
+            margin: auto;
+            transition: border-bottom-color 100ms ease-in, color 100ms ease-in;
           }
 
-          form > div.flex {
+          form div.flex {
             display: flex;
             flex-direction: row;
             width: 490px;
             align-items: center;
             justify-content: space-between;
           }
-
-          .email {
-            text-align: center;
-            margin: auto;
-            transition: border-bottom-color 100ms ease-in, color 100ms ease-in;
-            width: 100%;
-          }
-
-          .email.loading {
-            border-bottom-color: #888;
-          }
-
-          .email :global(button) {
-            height: 45px;
-            margin: 10px 0;
-            width: ${this.props.flex ? '175px' : '100%'};
-            background-color: ${this.props.flex ? 'var(--accents-1)' : 'var(--black)'};
-            color: ${this.props.flex ? 'var(--black)' : 'var(--white)'};
-            border: 1px solid #000;
-          }
-
-          .email :global(button:focus),
-          .email :global(button:hover) {
-            background-color: transparent;
-            border-color: ${this.props.flex ? 'var(--accents-1)' : 'var(--black)'};
-            color: ${this.props.flex ? 'var(--accents-1)' : 'var(--black)'};
-            outline: none;
-          }
-
-          .email.error label {
-            border-color: red;
-          }
-
-          label {
-            background-color: transparent;
-            display: flex;
-            border-radius: var(--radius);
-            border: 1px solid var(--accents-5);
-            align-items: center;
-            margin: 0 auto;
-            width: ${this.props.flex ? '275px' : '100%'};
-          }
-
-          .focus label {
-            background: none;
-            transition: border-color 100ms ease-in;
-            border-color: ${this.props.flex ? 'var(--white)' : 'var(--accents-8)'};
-          }
-
-          .error label {
-            border-color: red;
-          }
-
-          .icon {
-            margin: 0 14px;
-            display: inline-flex;
-          }
-
+          
           input {
             background-color: transparent;
-            border: none;
-            border-radius: 0;
             box-sizing: border-box;
-            color: ${this.props.flex ? 'var(--white)' : 'var(--black)'};
             display: block;
-            text-align: 'left';
+            text-align: left;
             font-size: 14px;
             font-family: var(--font-sans);
-            padding: 4px ${this.props.withIcon ? '0' : '10px'};
+            padding: 4px 10px;
             margin: 0 auto;
-            height: 37px;
+            height: 40px;
             line-height: 27px;
             width: 100%;
             box-shadow: none;
+            color: var(--black);
+            border: 1px solid var(--accents-5);
+            border-radius: var(--radius);
           }
 
-          .ios input {
-            font-size: 16px;
-            height: 34px;
-            padding-bottom: 4px;
+          input.flex {
+            color: var(--white);
           }
-
-          .focus {
-            border-bottom-color: #fff;
-          }
-
+          
           input::placeholder {
             color: #999;
             transition: color 100ms ease-in;
@@ -272,6 +74,13 @@ class EmailForm extends React.Component {
 
           input:focus {
             outline: none;
+            background: none;
+            transition: border-color 100ms ease-in;
+            border-color: var(--accents-8);
+          }
+
+          input.flex:focus {
+            border-color: var(--white);;
           }
 
           input:disabled {
@@ -282,112 +91,50 @@ class EmailForm extends React.Component {
             display: none;
           }
 
-          .shaking {
-            animation: shake 1s both;
-          }
-
-          .loading_anim {
-            display: block;
-            position: absolute;
-            bottom: 0px;
-            background: #eee;
-            width: 100%;
-            height: 1px;
-            animation-name: waiting;
-            animation-duration: 1s;
-            animation-direction: alternate;
-            animation-iteration-count: infinite;
-            animation-timing-function: cubic-bezier(0, 0, 1, 1);
-          }
-
-          .inverse .loading_anim {
-            background: #9b9b9b;
-          }
-
-          @keyframes waiting {
-            0% {
-              left: 0;
-              width: 50px;
-              opacity: 0;
-            }
-
-            50% {
-              width: 100px;
-              opacity: 1;
-              left: 50%;
-            }
-
-            100% {
-              width: 50px;
-              opacity: 0;
-              left: calc(100% - 50px);
-            }
-          }
-
-          .error-message {
-            position: absolute;
-            color: red;
-            text-align: center;
-            font-size: 12px;
-            width: 110%;
-            margin: 0;
-            margin-left: -5%;
-            margin-top: 0.5rem;
-          }
-
-          .error-message span {
+          button {
             position: relative;
-            padding-left: 20px;
+            display: inline-flex;
+            align-items: center;
+            justify-content: center;
+            cursor: pointer;
+            text-decoration: none;
+            padding: var(--gap-quarter) var(--gap-half);
+            margin: calc(-1 * var(--gap-quarter)) calc(-1 * var(--gap-half));
+            border-radius: var(--radius);
+            color: var(--accents-1);
+            background-color: var(--black);
+            border: 1px solid var(--black);
+            font-size: inherit;
+            line-height: inherit;
+            transition: background 0.2s ease, color 0.2s ease, box-shadow 0.2s ease;
+            outline: none;
+            height: 45px;
+            margin: 10px 0;
+            width: 100%;
+            color: var(--white);
           }
 
-          .error-message span::before {
-            display: block;
-            content: '';
-            width: 11px;
-            background: red;
-            border-radius: 2px;
-            height: 11px;
-            position: absolute;
-            left: 0;
-            top: 1px;
+          button.flex {
+            background-color: var(--accents-1);
+            color: var(--black);
+            width: 175px;
           }
 
-          .submit {
-            background: #fff;
-            margin: 0 auto;
-            display: block;
-            width: 200px;
-            height: 50px;
-            border: 2px solid #000;
-            text-transform: uppercase;
-            font-size: 12px;
-            margin-bottom: 50px;
-            font-weight: bold;
+          button:focus,
+          button:hover {
+            background-color: transparent;
+            outline: none;
+            border-color: var(--black);
+            color: var(--black);
           }
 
-          @keyframes shake {
-            0%,
-            100% {
-              transform: translate3d(0, 0, 0);
-            }
-
-            10%,
-            30%,
-            50%,
-            70%,
-            90% {
-              transform: translate3d(-10px, 0, 0);
-            }
-
-            20%,
-            40%,
-            60%,
-            80% {
-              transform: translate3d(10px, 0, 0);
-            }
+          button.flex:focus,
+          button.flex:hover {
+            border-color: var(--accents-1);
+            color: var(--accents-1);
           }
 
-          @media (max-width: 640px) {
+          @media (max-width: 700px) {
             form > div.flex {
               display: flex;
               flex-direction: column;
@@ -396,10 +143,10 @@ class EmailForm extends React.Component {
               justify-content: space-between;
             }
           }
-        `}</style>
-      </form>
-    )
-  }
-}
 
-export default EmailForm
+          </style>
+      </form>`
+      }}
+    />
+  )
+}
--- components/header/banner.js
+++ components/header/banner.js
@@ -1,26 +1,6 @@
-import { useState } from 'react'
 import EmailForm from '../email-form'
 
 const Banner = () => {
-  const [state, setState] = useState({ loading: false, success: false, errorMessage: false })
-
-  const onEmail = async val => {
-    setState({ loading: true })
-    const res = await fetch('/api/subscribe', {
-      body: JSON.stringify({ email: val }),
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      method: 'POST'
-    })
-
-    if (res.status === 200) {
-      setState({ loading: false, success: true })
-    } else {
-      setState({ loading: false, errorMessage: true })
-    }
-  }
-
   return (
     <div className="banner">
       <div className="inner">
@@ -28,24 +8,15 @@ const Banner = () => {
 
         <div className="email-form">
           <p className="mobile">Share your email with us for special offers</p>
-          <EmailForm
-            errorMessage={state.errorMessage}
-            loading={state.loading}
-            onEmail={onEmail}
-            buttonLabel="SUBSCRIBE"
-            message="Subscribe"
-            align="left"
-            withIcon
-            flex
-          />
+          <EmailForm buttonLabel="SUBSCRIBE" flex />
         </div>
       </div>
 
       <style jsx>{`
         .banner {
           width: 100%;
-          background: #000;
-          color: #fff;
+          background: var(--black);
+          color: var(--white);
           min-height: 75px;
           display: flex;
           align-items: center;
@@ -55,14 +26,14 @@ const Banner = () => {
           width: 100%;
           max-width: 1024px;
           margin: 0 auto;
-          padding: 0 1rem;
+          padding: 0 var(--gap);
           display: flex;
           align-items: center;
           justify-content: space-between;
         }
 
         p {
-          color: #fff;
+          color: var(--white);
           margin: 0;
           transition: color 0.15s ease;
         }
