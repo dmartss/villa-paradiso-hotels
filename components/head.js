ea0c431ecb88bad6da59ac48c46add6451bbe5a4
Dan <dmartss13@gmail.com>
2020-10-29 15:33:36 -0400

    font loading

--- components/head.js
+++ components/head.js
@@ -14,6 +14,14 @@ export default function Head({ title, children }) {
         }}
       />
       <NextHead>
+        <link
+          rel="preload"
+          href="https://assets.vercel.com/raw/upload/v1587415301/fonts/2/inter-var-latin.woff2"
+          as="font"
+          type="font/woff2"
+          crossOrigin="anonymous"
+        />
+
         {/* General */}
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <meta httpEquiv="Content-Language" content="en" />
@@ -28,14 +36,6 @@ export default function Head({ title, children }) {
         <link rel="mask-icon" href="/favicon/safari-pinned-tab.svg" color="#000000" />
         <meta name="msapplication-TileColor" content="#ffffff" />
         <meta name="theme-color" content="#ffffff" />
-
-        <link
-          rel="preload"
-          href="https://assets.vercel.com/raw/upload/v1587415301/fonts/2/inter-var-latin.woff2"
-          as="font"
-          type="font/woff2"
-          crossOrigin="anonymous"
-        />
 
         {children}
       </NextHead>
