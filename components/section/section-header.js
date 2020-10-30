5eac921a9254515b8e0def6fd9fcb4cb34c807dc
Dan <dmartss13@gmail.com>
2020-10-30 09:54:01 -0400

    more margin on home section header

--- components/section/section-header.js
+++ components/section/section-header.js
@@ -3,7 +3,10 @@ import styles from './section.module.css'
 
 export default function SectionHeader({ id, title, description }) {
   return (
-    <div className={cn(styles['section-header'], 'tc')} id={id}>
+    <div
+      className={cn('tc', styles['section-header'], { [styles.home]: id === 'welcome' })}
+      id={id}
+    >
       <h2 className="fp fw3">{title}</h2>
       {description && <p className="f-reset fs fw4">{description}</p>}
     </div>
